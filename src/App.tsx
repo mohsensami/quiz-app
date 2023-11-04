790bb63bb3cfafa502cfe5f392e090f24158666d
mohsensami <mohsensami88@gmail.com>
2023-11-04 22:09:55 +0330

    form begin componeted

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import shuffle from './utils/shuffle';
 import Layout from './componets/Layout';
+import FormBegin from './componets/FormBegin';
 
 function App() {
     const [amount, setAmount] = useState(10);
@@ -78,84 +79,17 @@ function App() {
                             </div>
                         </div>
                     ) : (
-                        <form onSubmit={handleSubmit}>
-                            <div>
-                                <label htmlFor="nums">Number of Questions:</label>
-                                <input
-                                    onChange={(e: any) => setAmount(e.target.value)}
-                                    value={amount}
-                                    type="number"
-                                    min={1}
-                                    max={50}
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor="trivia_category">Select Category: </label>
-                                <select
-                                    defaultValue={category}
-                                    onChange={(e: any) => setCategory(e.target.value)}
-                                    name="trivia_category"
-                                    className="form-control"
-                                >
-                                    <option value="any">Any Category</option>
-                                    <option value="9">General Knowledge</option>
-                                    <option value="10">Entertainment: Books</option>
-                                    <option value="11">Entertainment: Film</option>
-                                    <option value="12">Entertainment: Music</option>
-                                    <option value="13">Entertainment: Musicals &amp; Theatres</option>
-                                    <option value="14">Entertainment: Television</option>
-                                    <option value="15">Entertainment: Video Games</option>
-                                    <option value="16">Entertainment: Board Games</option>
-                                    <option value="17">Science &amp; Nature</option>
-                                    <option value="18">Science: Computers</option>
-                                    <option value="19">Science: Mathematics</option>
-                                    <option value="20">Mythology</option>
-                                    <option value="21">Sports</option>
-                                    <option value="22">Geography</option>
-                                    <option value="23">History</option>
-                                    <option value="24">Politics</option>
-                                    <option value="25">Art</option>
-                                    <option value="26">Celebrities</option>
-                                    <option value="27">Animals</option>
-                                    <option value="28">Vehicles</option>
-                                    <option value="29">Entertainment: Comics</option>
-                                    <option value="30">Science: Gadgets</option>
-                                    <option value="31">Entertainment: Japanese Anime &amp; Manga</option>
-                                    <option value="32">Entertainment: Cartoon &amp; Animations</option>{' '}
-                                </select>
-                            </div>
-                            <div>
-                                <label htmlFor="trivia_difficulty">Select Difficulty: </label>
-                                <select
-                                    onChange={(e: any) => setDifficulty(e.target.value)}
-                                    defaultValue={difficulty}
-                                    name="trivia_difficulty"
-                                    className="form-control"
-                                >
-                                    <option value="any">Any Difficulty</option>
-                                    <option value="easy">Easy</option>
-                                    <option value="medium">Medium</option>
-                                    <option value="hard">Hard</option>
-                                </select>
-                            </div>
-                            <div>
-                                <label htmlFor="trivia_type">Select Type: </label>
-                                <select
-                                    onChange={(e: any) => setType(e.target.value)}
-                                    defaultValue={type}
-                                    name="trivia_type"
-                                    className="form-control"
-                                >
-                                    &gt;
-                                    <option value="any">Any Type</option>
-                                    <option value="multiple">Multiple Choice</option>
-                                    <option value="boolean">True / False</option>
-                                </select>
-                            </div>
-                            <div>
-                                <button>Submit</button>
-                            </div>
-                        </form>
+                        <FormBegin
+                            handleSubmit={handleSubmit}
+                            amount={amount}
+                            setAmount={setAmount}
+                            category={category}
+                            setCategory={setCategory}
+                            type={type}
+                            setType={setType}
+                            difficulty={difficulty}
+                            setDifficulty={setDifficulty}
+                        />
                     )}
 
                     {isLoading && <h1>Loading ...</h1>}
--- src/componets/Header/index.jsx
+++ src/componets/Header/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = () => {
-    return <div>Header</div>;
+    return <div className="bg-cyan-500 py-4">Header</div>;
 };
 
 export default Header;
